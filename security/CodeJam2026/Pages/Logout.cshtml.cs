10ff68d40f9986d80e93d177f85a49052771b97c
Sasha <sashacrawford91@gmail.com>
2026-04-08 23:15:16 -0400

    CodeJam Pages Rename

--- security/CodeJam2026/Pages/Logout.cshtml.cs
+++ security/CodeJam2026/Pages/Logout.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace InvestorPrototype.Pages;
+namespace CodeJam2026.Pages;
 
 public class LogoutModel : PageModel
 {
