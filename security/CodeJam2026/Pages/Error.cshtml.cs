7fc8edeabbaa0c90054dbada09ffb4493115871d
Sasha <sashacrawford91@gmail.com>
2026-04-08 22:58:34 -0400

    Error Page Update

--- security/CodeJam2026/Pages/Error.cshtml.cs
+++ security/CodeJam2026/Pages/Error.cshtml.cs
@@ -1,11 +1,9 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CodeJam2026.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
     public string? RequestId { get; set; }
