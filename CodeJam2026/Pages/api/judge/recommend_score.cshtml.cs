cd8b7319f4fd14d2fa63b9fd73b3420c36c52cbb
HelixDelta <158601467+HelixDelta@users.noreply.github.com>
2026-04-18 12:42:49 -0400

    Pages/api folder structure setup
    The client should now be able to GET/POST and activate code inside of the cshtml.cs files.
    
    If I am not mistaken, the program gets the directories from what is stored in the file.cshtml file, and not exactly the location in the file heirarchy directory.  This has allowed me to setup the files to take in variables through the get/post methods through the directory called.
    
    I have been unable to test this setup to see if it works, so if someone with the means of running this in docker can test posts and gets, that would be neat.

---
+++ CodeJam2026/Pages/api/judge/recommend_score.cshtml.cs
@@ -0,0 +1,21 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql; 
+
+namespace CodeJam2026.Pages.api.judge;
+
+public class JudgeRecommendScoreModel : PageModel
+{
+    public async Task OnGetAsync()
+    {
+       
+    }
+
+    public async Task<IActionResult> OnPostAsync(int submission_id)
+    {
+        // This runs for:
+        // POST /api/judge/recommend_score/123
+    }
+    
+}
---
+++ CodeJam2026/Pages/api/scoreboard.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Npgsql; 
+
+namespace CodeJam2026.Pages.api;
+
+public class ScoreboardModel : PageModel
+{
+    public async Task OnGetAsync()
+    {
+       
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        
+    }
+    
+}
