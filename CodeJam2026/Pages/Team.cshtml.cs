bed5407295f84b3530dc6581ee871424c5d23901
Jayden Jimenez <jjimenez@wuglc.com>
2026-04-18 22:29:34 -0400

    Add session checks

--- CodeJam2026/Pages/Team.cshtml.cs
+++ CodeJam2026/Pages/Team.cshtml.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,18 +9,37 @@ namespace CodeJam2026.Pages;
 [Authorize(Roles = "Team")]
 public class TeamModel : PageModel
 {
+    private readonly ITeamSessionStore _teamSessionStore;
+
+    public TeamModel(ITeamSessionStore teamSessionStore)
+    {
+        _teamSessionStore = teamSessionStore;
+    }
+
     [BindProperty(SupportsGet = true)]
     public string teamName { get; set; } = "";
 
-    public IActionResult OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
         var loggedInTeam = User.FindFirst("TeamName")?.Value;
+        var cookieSessionId = User.FindFirst("TeamSessionId")?.Value;
 
-        if (string.IsNullOrWhiteSpace(loggedInTeam))
-            return Forbid();
+        if (string.IsNullOrWhiteSpace(loggedInTeam) || string.IsNullOrWhiteSpace(cookieSessionId))
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
+        }
 
         if (!string.Equals(loggedInTeam, teamName, StringComparison.OrdinalIgnoreCase))
             return Forbid();
+
+        var activeSessionId = _teamSessionStore.GetActiveSession(loggedInTeam);
+
+        if (activeSessionId != cookieSessionId)
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
+        }
 
         return Page();
     }
